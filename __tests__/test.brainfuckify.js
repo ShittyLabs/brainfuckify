0d9679136e83595823ee97dee5bc1dc727d2b86a
synthetic-borealis <snip.coloured@gmail.com>
2022-09-19 18:51:20 +0300

    Rewrite tests & add TypeScript definition generation

---
+++ __tests__/test.brainfuckify.js
@@ -0,0 +1,14 @@
+const {
+  compileToJsWeb,
+} = require('hirnfick');
+const brainfuckify = require('../lib/brainfuckify');
+
+describe('brainfuckify', () => {
+  it('Encodes text to correct brainfuck code', () => {
+    const inputText = 'Thank you for helping us help you help us all.';
+    const generatedBrainfuck = brainfuckify(inputText);
+    const generatedJavaScript = compileToJsWeb(generatedBrainfuck);
+    const generatedFunction = new Function(`${generatedJavaScript} return main().output;`);
+    expect(generatedFunction().trim()).toBe(inputText);
+  });
+});
