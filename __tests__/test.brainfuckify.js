f9939bbdd518adb161e308c4c0ef8c8262fb5c30
synthetic-borealis <snip.coloured@gmail.com>
2022-09-19 19:34:26 +0300

    Add a comment

--- __tests__/test.brainfuckify.js
+++ __tests__/test.brainfuckify.js
@@ -5,6 +5,7 @@ const brainfuckify = require('../lib/brainfuckify');
 
 describe('brainfuckify', () => {
   it('Encodes text to correct brainfuck code', () => {
+    // A GLaDOS quote
     const inputText = 'Thank you for helping us help you help us all.';
     const generatedBrainfuck = brainfuckify(inputText);
     const generatedJavaScript = compileToJsWeb(generatedBrainfuck);
